171374ab6ec8e60879746b2264337e0cd87c746f
Kirill Kononovich <kkononovich26@gmail.com>
2023-03-06 21:42:10 +0300

    PY-HW-13.2 | DONE

--- PY-HW-13.2.py
+++ PY-HW-13.2.py
@@ -1,23 +1,20 @@
-# 2. Вычислить факториал введённого числа.
-chislo = int(input("Введите число: "))
-factorial = 1
-
-while chislo > 1:
-    factorial = factorial * chislo
-    chislo -= 1
-print("Факториал введеного числа, равен:", factorial)
+# # 2. Вычислить факториал введённого числа.
+# chislo = int(input("Введите число: "))
+# factorial = 1
+#
+# while chislo > 1:
+#     factorial = factorial * chislo
+#     chislo -= 1
+# print("Факториал введеного числа, равен:", factorial)
 
 # 1. Вывести на экран столько элементов ряда Фибоначчи, сколько указал пользователь.
 # Например, если на ввод поступило число 6, то вывод должен содержать шесть первых чисел ряда Фибоначчи: 1 2 3 5 8 13.
 
 UsInput = int(input("Введите число: "))
-fib = 0
-fib2 = 1
+fib1 = fib2 = 1
 while UsInput >= 1:
-    # fib = fib2
-    # fib2 = fib + fib2
-    fib, fib2 = fib2, fib + fib2
-    print(fib2, end=" ")
-    UsInput -= 1
-
-
+    fib = fib1
+    fib1 = fib2
+    fib2 = fib + fib1
+    print(fib1, end=" ")
+    UsInput -=1
